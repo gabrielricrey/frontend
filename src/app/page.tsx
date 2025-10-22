1ff218a59be92f4ad90f5ab87ec3fbaa59c37970
Gabriel Reynoso <garey91@gmail.com>
2025-10-22 13:32:28 +0200

    bottom nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import axios from "axios";
 
 
 export default async function Home() {
-  const response = await axios.get("http://localhost:3000/property");
-  const { data: { properties: { data: properties } } } = response;
-
 
   return (
     <>

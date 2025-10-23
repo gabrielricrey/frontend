38db3f7a3572e16dd3cbb93b8d0141f66757e815
Gabriel Reynoso <garey91@gmail.com>
2025-10-23 15:17:35 +0200

    login page and form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/Login"
+
+const Page = () => {
+    return (
+        <div className="w-full flex justify-center items-center h-screen">
+            <Login />
+        </div>
+    )
+}
+
+export default Page

c86fb31fc2b838d37257b306aa66a350fb4e9ccc
Gabriel Reynoso <garey91@gmail.com>
2025-10-30 17:51:52 +0100

    block page for hosts

--- src/app/host/become/page.tsx
+++ src/app/host/become/page.tsx
@@ -1,9 +1,15 @@
 import BecomeHostButton from "@/components/BecomeHostButton"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 
 export default async function BecomeHost() {
+    const cookieStore = await cookies();
+    const hostMode = cookieStore.get("hostMode")?.value === 'true';
 
-
+    if (hostMode) {
+        redirect('/host');
+    };
 
     return (
         <div>

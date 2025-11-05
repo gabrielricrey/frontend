3ce178e50060e9c06dc4e6d309d61853cdb0d660
Gabriel Reynoso <garey91@gmail.com>
2025-11-06 00:12:09 +0100

    Refactor Hero component for improved text clarity and layout adjustments

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,21 @@
+import { HomeIcon } from "@heroicons/react/16/solid";
+
 export default function Hero() {
     return (
         <section
             className="relative flex items-center justify-center h-[50vh] overflow-hidden mt-16 shadow-lg"
         >
-            <div className="absolute inset-0 bg-linear-to-t from-black/0 via-black/10 to-transparent" />
+
 
             <div className="relative z-10 text-center text-black px-8">
                 <h1 className="text-4xl md:text-4xl font-bold mb-4 drop-shadow-lg">
-                    Find your next stay
+                    Find your next stay!
                 </h1>
-                <p className="text-lg md:text-lg italic mb-8 max-w-xl mx-auto text-black">
-                    Discover unique homes, cabins, and apartments around the world.
-                </p>
+                <p className="text-md italic mb-4">We provide what you need to enjoy your holiday with family</p>
+                <button className="rounded-2xl w-30 bg-black text-white font-medium p-2">Sign up</button>
+                {/* <div className="flex justify-center mt-2">
+                    <HomeIcon className="size-8" />
+                </div> */}
 
             </div>
         </section>

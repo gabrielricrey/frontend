644b3c9ebe26e4830400aaa7c1ca54920d66c1c5
Gabriel <garey91@gmail.com>
2026-02-14 17:51:05 +0100

    Style BecomeHostLabel

--- src/components/BecomeHostLabel.tsx
+++ src/components/BecomeHostLabel.tsx
@@ -1,9 +1,11 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function BecomeHostLabel() {
-    return (
-        <Link href={'/host/become'}>
-            <button className="border rounded-md p-2">Become a host</button>
-        </Link>
-    )
+  return (
+    <Link href="/host/become">
+      <button className="border rounded-md p-2 text-white bg-gradient-to-br from-blue-600 via-indigo-600 to-slate-900 hover:cursor-pointer">
+        Become a host
+      </button>
+    </Link>
+  );
 }

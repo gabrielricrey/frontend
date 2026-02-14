36c43c8c377dd290df521cb88fbcefdab20890bb
Gabriel <garey91@gmail.com>
2026-02-14 17:22:09 +0100

    Text color copyright

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+
+export default function Copyright() {
+    return (
+        <p className="text-sm text-white">
+            © {new Date().getFullYear()} Stay. All rights reserved.
+        </p>
+    )
+}

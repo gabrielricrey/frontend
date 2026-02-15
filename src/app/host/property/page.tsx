7427ed28beadd2a894ec55cb7a662cf43fd13ae3
Gabriel <garey91@gmail.com>
2026-02-15 13:23:29 +0100

    Refactor layout

--- src/app/host/property/page.tsx
+++ src/app/host/property/page.tsx
@@ -3,25 +3,29 @@ import HostProperties from "@/components/property/HostProperties";
 import Link from "next/link";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import SectionTitle from "@/components/SectionTitle";
 
 export default async function PropertiesPage() {
     const cookieStore = await cookies();
     const hostMode = cookieStore.get("hostMode")?.value === 'true';
 
     if (!hostMode) {
-
         redirect('/');
     }
+
     return (
-        <div className="min-h-screen flex items-start justify-center bg-gray-50 px-4">
-            <div className="w-full max-w-4xl mt-16 md:mt-20 bg-white p-4 rounded-2xl shadow-sm border border-gray-200 relative">
-                <Link
-                    href={'/host/property/create'}
-                    className="p-2 inline-flex border absolute right-5 top-5 rounded-lg text-white bg-blue-500 items-center gap-2"
-                >
-                    <PlusIcon className="size-5 text-white" />
-                    <span>Create Property</span>
-                </Link>
+        <div className="min-h-screen flex items-start justify-center bg-gray-50 px-4 py-8">
+            <div className="w-full max-w-4xl mt-16 md:mt-20 bg-white p-8 rounded-2xl shadow-sm border border-gray-200 relative">
+                <div className="flex justify-between items-start">
+                    <SectionTitle title={"Your properties"} />
+                    <Link
+                        href={'/host/property/create'}
+                        className="p-2 inline-flex border rounded-lg text-white bg-black hover:cursor-pointer items-center gap-2"
+                    >
+                        <PlusIcon className="size-5 text-white" />
+                        <span>Create Property</span>
+                    </Link>
+                </div>
                 <HostProperties />
             </div>
         </div >
